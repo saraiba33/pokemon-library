2f2f880fffa3b79bab12c8cf6ee148197ad56444
Sara Ibarra <saraiba33@gmail.com>
2022-04-01 22:43:41 -0600

    Add pokemon images

--- site/index.js
+++ site/index.js
@@ -1,22 +1,35 @@
-const getPokemon = () => {
-    const url = `https://pokeapi.co/api/v2/pokemon/pikachu`;
-    fetch(url)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data);
-            const pokemon = {
-                name: data.name,
-                id: data.id,
-                image: data.sprites['front_shiny'],
-                type: data.types.map(type => type.type.name).join()
-            };
+const pokedex = document.querySelector(".pokedex")
 
 
-            console.log(pokemon)
-        });
+const fetchPokemon = () => {
+    const promises = [];
+    for (let i = 1; i <= 50; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((response) => response.json()));
+    }
 
+    Promise.all(promises).then((results) => {
+        const pokemon = results.map((data) => ({
+            name: data.name,
+            id: data.id,
+            image: data.sprites['front_default'],
+            type: data.types.map((type) => type.type.name).join(', ')
+        }));
+        displayPokemon(pokemon);
+    });
 };
 
-getPokemon();
+const displayPokemon = (pokemon) => {
+    console.log(pokemon);
+    const pokemonHTMLString = pokemon.map(pokeman => `
+    <li>
+        <img src="${pokeman.image}"/>
+        <p>#0${pokeman.id}.</p>
+        <h2>${pokeman.name}</h2>
+        <p>Type: ${pokeman.type}</p>
+    </li>
+    `);
+    pokedex.innerHTML = pokemonHTMLString
+};
+
+fetchPokemon();
